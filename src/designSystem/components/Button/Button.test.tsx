8b8e88d07555b1e330bf34b283e50929d1805392
Chris McLean <havok2905@gmail.com>
2024-06-24 16:13:55 -0600

    added button component

--- src/designSystem/components/Button/Button.test.tsx
+++ src/designSystem/components/Button/Button.test.tsx
@@ -6,11 +6,241 @@ import { mount } from '@shopify/react-testing';
 import { Button } from './Button';
 
 describe('Button', () => {
-  it('should render', () => {
+  it('should render as a button by default', () => {
     const wrapper = mount(
       <Button buttonText="Click me" />
     );
 
-    expect(wrapper.text()).toEqual('Click me');
+    const button = wrapper.find('button');
+
+    expect(button?.text()).toEqual('Click me');
+  });
+
+  it('should render as a button as button', () => {
+    const wrapper = mount(
+      <Button
+        as="button"
+        buttonText="Click me" />
+    );
+
+    const button = wrapper.find('button');
+
+    expect(button?.text()).toEqual('Click me');
+  });
+
+  it('should render as a anchor as a', () => {
+    const wrapper = mount(
+      <Button
+        as="a"
+        buttonText="Click me" />
+    );
+
+    const button = wrapper.find('a');
+
+    expect(button?.text()).toEqual('Click me');
+  });
+
+  it('should pass along disabled state as button', () => {
+    const wrapper = mount(
+      <Button
+        buttonText="Click me"
+        disabled />
+    );
+
+    const button = wrapper.find('button');
+
+    expect(button?.props.disabled).toEqual(true);
+  });
+
+  it('should pass along enabled state as button', () => {
+    const wrapper = mount(
+      <Button
+        buttonText="Click me"
+        disabled={false} />
+    );
+
+    const button = wrapper.find('button');
+
+    expect(button?.props.disabled).toEqual(false);
+  });
+
+  it('should pass along disabled state as anchor', () => {
+    const wrapper = mount(
+      <Button
+        as="a"
+        buttonText="Click me"
+        disabled />
+    );
+
+    const button = wrapper.find('a');
+
+    expect(button?.props.className?.includes('button-disabled')).toEqual(true);
+  });
+
+  it('should pass along enabled state as anchor', () => {
+    const wrapper = mount(
+      <Button
+        as="a"
+        buttonText="Click me"
+        disabled={false} />
+    );
+
+    const button = wrapper.find('a');
+
+    expect(button?.props.className?.includes('button-disabled')).toEqual(false);
+  });
+
+  it('should fire passed along callbacks as button', () => {
+    const onBlur = jest.fn();
+    const onClick = jest.fn();
+    const onFocus = jest.fn();
+    const onKeyDown = jest.fn();
+    const onKeyUp = jest.fn();
+    const onMouseEnter = jest.fn();
+    const onMouseLeave = jest.fn();
+
+    const wrapper = mount(
+      <Button
+        buttonText="Click me"
+        disabled={false}
+        onBlur={onBlur}
+        onClick={onClick}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave} />
+    );
+
+    const button = wrapper.find('button');
+
+    button?.trigger('onBlur', {});
+    button?.trigger('onClick', {});
+    button?.trigger('onFocus', {});
+    button?.trigger('onKeyDown', {});
+    button?.trigger('onKeyUp', {});
+    button?.trigger('onMouseEnter', {});
+    button?.trigger('onMouseLeave', {});
+
+    expect(onBlur).toHaveBeenCalledTimes(1);
+    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onKeyDown).toHaveBeenCalledTimes(1);
+    expect(onKeyUp).toHaveBeenCalledTimes(1);
+    expect(onMouseEnter).toHaveBeenCalledTimes(1);
+    expect(onMouseLeave).toHaveBeenCalledTimes(1);
+  });
+
+  it('should fire passed along callbacks as anchor', () => {
+    const onBlur = jest.fn();
+    const onClick = jest.fn();
+    const onFocus = jest.fn();
+    const onKeyDown = jest.fn();
+    const onKeyUp = jest.fn();
+    const onMouseEnter = jest.fn();
+    const onMouseLeave = jest.fn();
+
+    const onClickPreventDefault = jest.fn();
+    const onKeyDownPreventDefault = jest.fn();
+    const onKeyUpPreventDefault = jest.fn();
+
+    const wrapper = mount(
+      <Button
+        as="a"
+        buttonText="Click me"
+        disabled={false}
+        onBlur={onBlur}
+        onClick={onClick}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave} />
+    );
+
+    const button = wrapper.find('a');
+
+    button?.trigger('onBlur', {});
+    button?.trigger('onClick', {
+      preventDefault: onClickPreventDefault
+    });
+    button?.trigger('onFocus', {});
+    button?.trigger('onKeyDown', {
+      preventDefault: onKeyDownPreventDefault
+    });
+    button?.trigger('onKeyUp', {
+      preventDefault: onKeyUpPreventDefault
+    });
+    button?.trigger('onMouseEnter', {});
+    button?.trigger('onMouseLeave', {});
+
+    expect(onBlur).toHaveBeenCalledTimes(1);
+    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onKeyDown).toHaveBeenCalledTimes(1);
+    expect(onKeyUp).toHaveBeenCalledTimes(1);
+    expect(onMouseEnter).toHaveBeenCalledTimes(1);
+    expect(onMouseLeave).toHaveBeenCalledTimes(1);
+
+    expect(onClickPreventDefault).toHaveBeenCalledTimes(1);
+    expect(onKeyDownPreventDefault).toHaveBeenCalledTimes(1);
+    expect(onKeyUpPreventDefault).toHaveBeenCalledTimes(1);
+  });
+
+  it('should fire passed along callbacks and not call preventDefault when href is present as anchor', () => {
+    const onBlur = jest.fn();
+    const onClick = jest.fn();
+    const onFocus = jest.fn();
+    const onKeyDown = jest.fn();
+    const onKeyUp = jest.fn();
+    const onMouseEnter = jest.fn();
+    const onMouseLeave = jest.fn();
+
+    const onClickPreventDefault = jest.fn();
+    const onKeyDownPreventDefault = jest.fn();
+    const onKeyUpPreventDefault = jest.fn();
+
+    const wrapper = mount(
+      <Button
+        as="a"
+        buttonText="Click me"
+        disabled={false}
+        href="#"
+        onBlur={onBlur}
+        onClick={onClick}
+        onFocus={onFocus}
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave} />
+    );
+
+    const button = wrapper.find('a');
+
+    button?.trigger('onBlur', {});
+    button?.trigger('onClick', {
+      preventDefault: onClickPreventDefault
+    });
+    button?.trigger('onFocus', {});
+    button?.trigger('onKeyDown', {
+      preventDefault: onKeyDownPreventDefault
+    });
+    button?.trigger('onKeyUp', {
+      preventDefault: onKeyUpPreventDefault
+    });
+    button?.trigger('onMouseEnter', {});
+    button?.trigger('onMouseLeave', {});
+
+    expect(onBlur).toHaveBeenCalledTimes(1);
+    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onKeyDown).toHaveBeenCalledTimes(1);
+    expect(onKeyUp).toHaveBeenCalledTimes(1);
+    expect(onMouseEnter).toHaveBeenCalledTimes(1);
+    expect(onMouseLeave).toHaveBeenCalledTimes(1);
+
+    expect(onClickPreventDefault).toHaveBeenCalledTimes(0);
+    expect(onKeyDownPreventDefault).toHaveBeenCalledTimes(0);
+    expect(onKeyUpPreventDefault).toHaveBeenCalledTimes(0);
   });
 });
--- src/designSystem/components/Button/Button.tsx
+++ src/designSystem/components/Button/Button.tsx
@@ -1,14 +1,150 @@
+import {
+  FocusEvent,
+  KeyboardEvent,
+  MouseEvent
+} from 'react';
+import classNames from 'classnames';
+
 import './Button.css';
 
 export interface ButtonProps {
+  as?: 'button' | 'a';
   buttonText: string;
+  disabled?: boolean;
+  href?: string;
+  onBlur?: (e: FocusEvent) => void;
+  onClick?: (e: MouseEvent) => void;
+  onFocus?: (e: FocusEvent) => void;
+  onKeyDown?: (e: KeyboardEvent) => void;
+  onKeyUp?: (e: KeyboardEvent) => void;
+  onMouseEnter?: (e: MouseEvent) => void;
+  onMouseLeave?: (e: MouseEvent) => void;
 }
 
 export const Button = ({
-  buttonText
+  as = 'button',
+  buttonText,
+  disabled = false,
+  href,
+  onBlur,
+  onClick,
+  onFocus,
+  onKeyDown,
+  onKeyUp,
+  onMouseEnter,
+  onMouseLeave
 }: ButtonProps) => {
+  const isLinkWithoutHref = as === 'a' && !href;
+
+  const handleOnBlur = (e: FocusEvent) => {
+    if (onBlur) {
+      onBlur(e);
+    }
+  };
+
+  const handleOnClick = (e: MouseEvent) => {
+    if (isLinkWithoutHref) {
+      e.preventDefault();
+    }
+
+    if (onClick) {
+      onClick(e);
+    }
+  };
+
+  const handleOnFocus = (e: FocusEvent) => {
+    if (onFocus) {
+      onFocus(e);
+    }
+  };
+
+
+  const handleOnKeyDown = (e: KeyboardEvent) => {
+    if (isLinkWithoutHref) {
+      e.preventDefault();
+    }
+
+    if (onKeyDown) {
+      onKeyDown(e);
+    }
+  };
+
+  const handleOnKeyUp = (e: KeyboardEvent) => {
+    if (isLinkWithoutHref) {
+      e.preventDefault();
+    }
+
+    if (onKeyUp) {
+      onKeyUp(e);
+    }
+  };
+
+  const handleOnMouseEnter = (e: MouseEvent) => {
+    if(onMouseEnter) {
+      onMouseEnter(e);
+    }
+  };
+
+  const handleOnMouseLeave = (e: MouseEvent) => {
+    if(onMouseLeave) {
+      onMouseLeave(e);
+    }
+  };
+
+  if (as === 'a') {
+    const classList = {
+      'button': true,
+      'button-disabled': disabled
+    };
+
+    if (href) {
+      return (
+        <a
+          className={classNames(classList)}
+          href={href || undefined}
+          onBlur={handleOnBlur}
+          onClick={handleOnClick}
+          onFocus={handleOnFocus}
+          onKeyDown={handleOnKeyDown}
+          onKeyUp={handleOnKeyUp}
+          onMouseEnter={handleOnMouseEnter}
+          onMouseLeave={handleOnMouseLeave}
+          role={href ? undefined : 'button'}
+          tabIndex={0}>
+          {buttonText}
+        </a>
+      );
+    }
+
+    return (
+      <a
+        className={classNames(classList)}
+        onBlur={handleOnBlur}
+        onClick={handleOnClick}
+        onFocus={handleOnFocus}
+        onKeyDown={handleOnKeyDown}
+        onKeyUp={handleOnKeyUp}
+        onMouseEnter={handleOnMouseEnter}
+        onMouseLeave={handleOnMouseLeave}
+        role="button"
+        tabIndex={0}>
+        {buttonText}
+      </a>
+    );
+  }
+
   return (
-    <button className="button">
+    <button
+      className="button"
+      disabled={disabled}
+      onBlur={handleOnBlur}
+      onClick={handleOnClick}
+      onFocus={handleOnFocus}
+      onKeyDown={handleOnKeyDown}
+      onKeyUp={handleOnKeyUp}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+      role="button">
       {buttonText}
     </button>
   );
