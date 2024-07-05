817a50d3baacda06dace1fc4aeb93c93cf873832
Chris McLean <havok2905@gmail.com>
2024-07-04 22:06:20 -0600

    show player handouts on player view page

--- src/dm-screen/components/PlayerView/PlayerView.tsx
+++ src/dm-screen/components/PlayerView/PlayerView.tsx
@@ -1,10 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
 import { Container } from '@designSystem/components';
+import { Adventure } from '../../../core/types';
 
 export const PlayerView = () => {
+  const {
+    data,
+    isFetching,
+    isLoading,
+    isPending
+  } = useQuery({
+    queryKey: ['adventureData'],
+    queryFn: () => {
+      return fetch('http://localhost:3000/adventure/1').then((response) => response.json())
+    }  
+  });
+
+  if (
+    isFetching ||
+    isLoading ||
+    isPending
+  ) return null;
+
+  if (!data) {
+    return null;
+  }
+
+  const adventure = data as Adventure;
+
   return (
     <>
       <Container>
         <h1>Player View</h1>
+        {
+          adventure.handouts.map((item) => {
+            return (
+              <img
+                alt={item.description}
+                key={item.id}
+                src={item.url}
+              />
+            )
+          })
+        }
       </Container>
     </>
   )
