1d81b86faca0875354f36a96ed0d6cee84386c18
Chris McLean <havok2905@gmail.com>
2024-07-15 01:28:16 -0600

    added tests

--- src/dm-screen/components/InitiativeOrderComponent/InitiativeOrderDmControls.test.tsx
+++ src/dm-screen/components/InitiativeOrderComponent/InitiativeOrderDmControls.test.tsx
@@ -16,6 +16,7 @@ describe('InitiativeOrderDmControls', () => {
       <InitiativeOrderDmControls
         handleBootstrapInitiativeClick={handleBootstrapInitiativeClick}
         handleRemoveInitiativeClick={handleRemoveInitiativeClick}
+        inInCombat={false}
         isPlayer={false}
       />
     );
@@ -23,11 +24,32 @@ describe('InitiativeOrderDmControls', () => {
     const buttons = wrapper.findAll('button');
 
     buttons[0].trigger('onClick');
-    buttons[1].trigger('onClick');
 
-    expect(handleRemoveInitiativeClick).toHaveBeenCalledTimes(1);
+    expect(handleBootstrapInitiativeClick).toHaveBeenCalledTimes(1);
+    expect(handleRemoveInitiativeClick).toHaveBeenCalledTimes(0);
+  });
+
+  it('should render in combat', () => {
+    const handleBootstrapInitiativeClick = jest.fn();
+    const handleRemoveInitiativeClick = jest.fn();
+
+    const wrapper = mount(
+      <InitiativeOrderDmControls
+        handleBootstrapInitiativeClick={handleBootstrapInitiativeClick}
+        handleRemoveInitiativeClick={handleRemoveInitiativeClick}
+        inInCombat={true}
+        isPlayer={false}
+      />
+    );
+
+    const buttons = wrapper.findAll('button');
+
+    buttons[0].trigger('onClick');
+
+    expect(handleBootstrapInitiativeClick).toHaveBeenCalledTimes(0);
     expect(handleRemoveInitiativeClick).toHaveBeenCalledTimes(1);
   });
+
 
   it('should render null when it is a player', () => {
     const handleBootstrapInitiativeClick = jest.fn();
