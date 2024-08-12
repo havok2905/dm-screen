6a03d46bb0e7f0a535b29ad3094bf0fc225f3d26
Chris McLean <mclean.webdev@gmail.com>
2024-08-12 06:57:20 -0600

    added toggle behavior for items and creatures on adventure page

--- src/dm-screen/pages/Compendium/AdventurePage/AdventurePage.test.tsx
+++ src/dm-screen/pages/Compendium/AdventurePage/AdventurePage.test.tsx
@@ -25,48 +25,88 @@ jest.mock('../../../hooks', () => ({
   useAdventure: jest.fn()
 }));
 
+const mockAdventure = {
+  data: {
+    creatures: [
+      {
+        content: `# Hello, creature!`,
+        id: '2',
+        image: 'creature.png',
+        metadata: [
+          {
+            name: 'Type',
+            type: 'string',
+            value: 'Beast'
+          },
+          {
+            name: 'AC',
+            type: 'number',
+            value: 15
+          },
+          {
+            name: 'HP',
+            type: 'number',
+            value: 25
+          },
+          {
+            name: 'CR',
+            type: 'string',
+            value: '1/2'
+          }
+        ],
+        name: 'Test Creature'
+      }
+    ],
+    handouts: [
+      {
+        description: 'image description',
+        id: '4',
+        name: 'Test Image',
+        url: '/'
+      }
+    ],
+    id: '1',
+    items: [
+      {
+        content: `# Hello, item!`,
+        id: '3',
+        image: 'item.png',
+        metadata: [
+          {
+            name: 'Rarity',
+            type: 'string',
+            value: 'Common'
+          },
+          {
+            name: 'Cost',
+            type: 'string',
+            value: '2GP'
+          }
+        ],
+        name: 'Test Item'
+      }
+    ],
+    name: 'Test Adventure',
+    notes: `# Hello, world!
+
+Lorem ipsum
+`,
+    system: 'Test System',
+
+  },
+  isFetching: false,
+  isLoading: false,
+  isPending: false,
+  isSuccess: true
+};
+
 describe('AdventurePage', () => {
   it('should render', () => {
     const queryClient = new QueryClient();
 
     // @ts-expect-error This is a Jest mock
     useAdventure.mockImplementation(() => {
-      return {
-        data: {
-          creatures: [
-            {
-              id: '2',
-              name: 'Test Creature'
-            }
-          ],
-          handouts: [
-            {
-              description: 'image description',
-              id: '4',
-              name: 'Test Image',
-              url: '/'
-            }
-          ],
-          id: '1',
-          items: [
-            {
-              id: '3',
-              name: 'Test Item'
-            }
-          ],
-          name: 'Test Adventure',
-          notes: `# Hello, world!
-
-Lorem ipsum
-`,
-          system: 'Test System',
-
-        },
-        isFetching: false,
-        isLoading: false,
-        isPending: false,
-        isSuccess: true
-      };
+      return mockAdventure;
     });
 
     const wrapper = mount(
@@ -160,5 +200,85 @@ Lorem ipsum
     expect(wrapper).not.toContainReactText('Creatures');
     expect(wrapper).not.toContainReactText('Items');
     expect(wrapper).not.toContainReactText('Handouts');
+  });
+
+  it('should toggle creatures', () => {
+    const queryClient = new QueryClient();
+
+    // @ts-expect-error This is a Jest mock
+    useAdventure.mockImplementation(() => {
+      return mockAdventure;
+    });
+
+    const wrapper = mount(
+      <QueryClientProvider client={queryClient}>
+        <AdventurePage />
+      </QueryClientProvider>
+    );
+
+    let creatureToggle;
+    
+    creatureToggle = wrapper.find('table')?.find('button');
+    expect(creatureToggle).toContainReactText('Expand');
+    expect(wrapper).not.toContainReactText('Hello, creature!');
+    expect(wrapper).not.toContainReactText('Type: Beast');
+    expect(wrapper).not.toContainReactText('AC: 15');
+    expect(wrapper).not.toContainReactText('HP: 25');
+    expect(wrapper).not.toContainReactText('CR: 1/2');
+    creatureToggle?.trigger('onClick');
+
+    creatureToggle = wrapper.find('table')?.find('button');
+    expect(creatureToggle).toContainReactText('Collapse');
+    expect(wrapper).toContainReactText('Hello, creature!');
+    expect(wrapper).toContainReactText('Type: Beast');
+    expect(wrapper).toContainReactText('AC: 15');
+    expect(wrapper).toContainReactText('HP: 25');
+    expect(wrapper).toContainReactText('CR: 1/2');
+    creatureToggle?.trigger('onClick');
+
+    creatureToggle = wrapper.find('table')?.find('button');
+    expect(creatureToggle).toContainReactText('Expand');
+    expect(wrapper).not.toContainReactText('Hello, creature!');
+    expect(wrapper).not.toContainReactText('Type: Beast');
+    expect(wrapper).not.toContainReactText('AC: 15');
+    expect(wrapper).not.toContainReactText('HP: 25');
+    expect(wrapper).not.toContainReactText('CR: 1/2');
+  });
+
+  it('should toggle items', () => {
+    const queryClient = new QueryClient();
+
+    // @ts-expect-error This is a Jest mock
+    useAdventure.mockImplementation(() => {
+      return mockAdventure;
+    });
+
+    const wrapper = mount(
+      <QueryClientProvider client={queryClient}>
+        <AdventurePage />
+      </QueryClientProvider>
+    );
+
+    let itemToggle;
+    
+    itemToggle = wrapper.findAll('table')[1]?.find('button');
+    expect(itemToggle).toContainReactText('Expand');
+    expect(wrapper).not.toContainReactText('Hello, item!');
+    expect(wrapper).not.toContainReactText('Rarity: Common');
+    expect(wrapper).not.toContainReactText('Cost: 2GP');
+    itemToggle?.trigger('onClick');
+
+    itemToggle = wrapper.findAll('table')[1]?.find('button');
+    expect(itemToggle).toContainReactText('Collapse');
+    expect(wrapper).toContainReactText('Hello, item!');
+    expect(wrapper).toContainReactText('Rarity: Common');
+    expect(wrapper).toContainReactText('Cost: 2GP');
+    itemToggle?.trigger('onClick');
+
+    itemToggle = wrapper.findAll('table')[1]?.find('button');
+    expect(itemToggle).toContainReactText('Expand');
+    expect(wrapper).not.toContainReactText('Hello, item!');
+    expect(wrapper).not.toContainReactText('Rarity: Common');
+    expect(wrapper).not.toContainReactText('Cost: 2GP');
   });
 });
